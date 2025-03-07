e8d57ba030ca847d2a8525a4655e08db41e22528
Zehra Seren <fatmazehraseren@gmail.com>
2025-03-07 20:34:34 +0300

    Initialize Identity Microservice

---
+++ IdentityServer/MS.IdentityServer/Controller/RegistersController.cs
@@ -0,0 +1,42 @@
+﻿using MS.IdentityServer.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using MS.IdentityServer.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace MS.IdentityServer.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RegistersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UserRegister(UserRegisterDto urdto)
+        {
+            var values = new ApplicationUser()
+            {
+                UserName = urdto.UserName,
+                Email = urdto.Email,
+                Name = urdto.Name,
+                Surname = urdto.Surname
+            };
+
+            var result = await _userManager.CreateAsync(values, urdto.Password);
+
+            if (result.Succeeded)
+            {
+                return Ok("Kullanıcı başarıyla eklendi.");
+            }
+            else
+            {
+                return Ok("Bir hata oluştu tekrar deneyiniz.");
+            }
+        }
+    }
+}
---
+++ IdentityServer/MS.IdentityServer/Dtos/UserRegisterDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MS.IdentityServer.Dtos
+{
+    public class UserRegisterDto
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Password { get; set; }
+    }
+}
