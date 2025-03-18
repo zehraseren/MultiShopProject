40b05fd8d72f7a702cbca0a4ab65c4fd9851a0f2
Zehra Seren <fatmazehraseren@gmail.com>
2025-03-18 03:33:42 +0300

    UI Layout

---
+++ Frontends/MS.WebUI/Controllers/UILayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MS.WebUI.Controllers
+{
+    public class UILayoutController : Controller
+    {
+        public IActionResult _IULayout()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/MS.WebUI/ViewComponents/DefaultViewComponents/_CategoriesDefaultComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MS.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class _CategoriesDefaultComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
