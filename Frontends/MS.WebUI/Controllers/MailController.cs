57a1ccffffa40ea3a929dac4f75062eae085b98b
Zehra Seren <fatmazehraseren@gmail.com>
2025-06-07 23:39:44 +0300

    Implemented email sending functionality with basic configuration and contact form integration.

---
+++ Frontends/MS.WebUI/Controllers/MailController.cs
@@ -0,0 +1,40 @@
+﻿using MimeKit;
+using MS.WebUI.Models;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MS.WebUI.Controllers;
+
+public class MailController : Controller
+{
+    [HttpGet]
+    public IActionResult SendMail()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult SendMail(MailRequestViewModel requestvm)
+    {
+        MimeMessage mimeMessage = new MimeMessage();
+
+        MailboxAddress mailboxAddressFrom = new MailboxAddress("MultiShop Katalog", "fatmazehraseren@gmail.com");
+        mimeMessage.From.Add(mailboxAddressFrom);
+
+        MailboxAddress mailboxAddressTo = new MailboxAddress("User", requestvm.ReceiverMail);
+        mimeMessage.To.Add(mailboxAddressTo);
+
+        var bodyBuilder = new BodyBuilder();
+        bodyBuilder.TextBody = requestvm.MailContent;
+        mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+        mimeMessage.Subject = requestvm.Subject;
+
+        SmtpClient smtpClient = new SmtpClient();
+        smtpClient.Connect("smtp.gmail.com", 587, false);
+        smtpClient.Authenticate("fatmazehraseren@gmail.com", "lrarucaygulndcnf");
+        smtpClient.Send(mimeMessage);
+        smtpClient.Disconnect(true);
+        return View();
+    }
+}
---
+++ Frontends/MS.WebUI/Models/MailRequestViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MS.WebUI.Models;
+
+public class MailRequestViewModel
+{
+    public string ReceiverMail { get; set; }
+    public string Subject { get; set; }
+    public string MailContent { get; set; }
+}
